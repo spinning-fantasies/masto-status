df78936b5b95e392aa868a45b7e2e117d40c79c0
Mathieu Mescam <mate@e.email>
2023-08-19 15:46:53 +0200

    update body of the message

--- masto_status_toot.py
+++ masto_status_toot.py
@@ -4,10 +4,8 @@ import os.path
 
 api_token = open(os.path.expanduser(".secret/api_key"), "r")  # text file with a string
 api_token = api_token.readline().strip()
-print(api_token)
 
 req=requests.get("https://3615.computer/api/v1/instance") # change the url
-print
 jsondata=json.loads(req.text)
 users=jsondata['stats']['user_count']
 posts=jsondata['stats']['status_count']
@@ -18,15 +16,16 @@ jsoninst = json.loads(instance.text)
 active = jsoninst['usage']['users']['active_month']
 mastoversion = jsoninst['version']
 
-
-
 # add date !
-data = {'status' :"""
+data = {
+    'visibility' : "public",
+    'status' :"""
 
 User accounts: {}
 User posts   : {}
 Active users : {}
 Domains : {}        
+
 #3615computerstatus""".format(users,posts,active,domains)}  # this is the body of the message
 
 toot_url = "https://3615.computer/api/v1/statuses" # change the url here, too.
